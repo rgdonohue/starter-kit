821b38dcea83c85d8fdef00dff57688c53b37fe4
Rich Donohue <rgdonohue@gmail.com>
2017-06-16 12:25:38 -0600

    add mock api

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,14 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res){
+  res.json([
+    {'id': 1, "firstName": "bob", "lastName": "smith", "email": "jsmith@gmail.com"},
+    {'id': 2, "firstName": "susan", "lastName": "dodgers", "email": "jsth@gmail.com"},
+    {'id': 3, "firstName": "ted", "lastName": "frank", "email": "jsmh@gmail.com"}
+  ])
+});
+
 app.listen(port, function(err){
   if (err) {
     console.log(err);
--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,3 @@
 import chalk from 'chalk';
 /* eslint-disable no-console */
-console.log(chalk.green('we are starting something'));
+console.log(chalk.green('gunna be starting something'));
