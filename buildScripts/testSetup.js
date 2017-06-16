092917816d1557f063da19b3e9ab053ec29971d8
Rich Donohue <rgdonohue@gmail.com>
2017-06-16 09:20:18 -0600

    add first 10 modules

---
+++ buildScripts/testSetup.js
@@ -0,0 +1,6 @@
+
+// register babel to transpile before our tests run
+require('babel-register')();
+
+// disable webpack features that Mocha doesn't understand
+require.extensions['css'] = function()  {};
